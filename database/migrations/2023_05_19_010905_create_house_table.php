5f6694321b459c1341ef1d0ccd4debfb1f9b2787
rsydfhmy03 <fahmyrosyadi29@gmail.com>
2023-05-19 08:32:02 +0700

    fixed(tidy up assets), feat : add schema

---
+++ database/migrations/2023_05_19_010905_create_house_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHouseTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('house', function (Blueprint $table) {
+            $table->id();
+            $table->string('type',10);
+            $table->integer('price');
+            $table->enum('status',['Tersedia','Tidak Tersedia'])->default('Tersedia');
+            $table->string('photo');
+            $table->string('keterangan');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('house');
+    }
+}
