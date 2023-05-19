9d22927104445c4a08e78c2d839701a7911c5131
rsydfhmy03 <fahmyrosyadi29@gmail.com>
2023-05-19 10:52:47 +0700

    Finish(API)

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HouseApiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/houses', [HouseApiController::class, 'index']);
+Route::post('/houses', [HouseApiController::class, 'store']);
+Route::get('/houses/{id}', [HouseApiController::class, 'show']);
+Route::put('/houses/{id}', [HouseApiController::class, 'update']);
+Route::delete('/houses/{id}', [HouseApiController::class, 'destroy']);
